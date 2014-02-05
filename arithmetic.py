0d426afe2ef4b5af7c3fe68eab8286d51f65b5fa
Hackbright Student <user@mini-002.(none)>
2014-02-05 12:54:01 -0800

    Fixed division to return float

--- arithmetic.py
+++ arithmetic.py
@@ -3,30 +3,21 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     return num1 - num2
-    
-#subtract(8,3)
 
 def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return num1 / num2
+    return float(num1) / float(num2)
 
 def square(num1):
     return num1 * num1
 
-#print square(5)
-
 def cube(num1):
     return num1 ** 3
 
-#print cube(3)
-
 def power(num1, num2):
     return num1 ** num2
-#print power(2, 3)
 
 def mod(num1, num2):
     return num1 % num2
-
-#print mod(7, 2)
